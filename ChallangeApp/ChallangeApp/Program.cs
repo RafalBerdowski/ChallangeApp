f8fdb9d933e78f296c1e547a3fa7d1c83f87077f
RafalBerdowski <voltenbygg@gmail.com>
2023-08-01 23:49:45 +0200

    Zadanie dzien 4.

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,19 +1,39 @@
-﻿// Homework day 4.
+﻿int number = 992373456;
 
-string name = "Rafal";
-var age = 17;
-bool women = false;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
 
-if (women && age < 30 == true)
-{
-    Console.WriteLine("Kobieta ponizej 30 lat");
-}
-else if (women && age == 33 && name == "Ewa")
-{
-    Console.WriteLine("Ewa lat 33");
-}
-else if (!women && age < 18 && name != "Ewa")
-{
-    Console.WriteLine("Niepelnoletni mezczyzna");
-}
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
+foreach (char c in letters)
+{
+    if (c == '0') {counter0++; }
+    else if (c == '1') { counter1++; }
+    else if (c == '2') { counter2++; }
+    else if (c == '3') { counter3++; }
+    else if (c == '4') { counter4++; }
+    else if (c == '5') { counter5++; }
+    else if (c == '6') { counter6++; }
+    else if (c == '7') { counter7++; }
+    else if (c == '8') { counter8++; }
+    else if (c == '9') { counter9++; }
+}
+Console.WriteLine("0 => " + counter0);
+Console.WriteLine("1 => " + counter1);
+Console.WriteLine("2 => " + counter2);
+Console.WriteLine("3 => " + counter3);
+Console.WriteLine("4 => " + counter4);
+Console.WriteLine("5 => " + counter5);
+Console.WriteLine("6 => " + counter6);
+Console.WriteLine("7 => " + counter7);
+Console.WriteLine("8 => " + counter8);
+Console.WriteLine("9 => " + counter9);
